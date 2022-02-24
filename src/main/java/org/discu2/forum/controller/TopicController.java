db3abaab0cc3183850038bf0695e681fe60a1708
asdkmm5050 <asdkmm505@gmail.com>
2022-02-25 02:55:30 +0800

    implements basic TopicController functions (postTopic, replyTopic, commentPost and commentReply).

--- src/main/java/org/discu2/forum/controller/TopicController.java
+++ src/main/java/org/discu2/forum/controller/TopicController.java
@@ -4,7 +4,9 @@ import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import org.discu2.forum.model.TextBlock;
 import org.discu2.forum.model.Topic;
+import org.discu2.forum.repository.CommentRepository;
 import org.discu2.forum.repository.PostRepository;
+import org.discu2.forum.repository.ReplyRepository;
 import org.discu2.forum.repository.TopicRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,8 @@ public class TopicController {
 
     private final TopicRepository topicRepository;
     private final PostRepository postRepository;
+    private final ReplyRepository replyRepository;
+    private final CommentRepository commentRepository;
 
     @PostMapping("/post_topic")
     public String postTopic(@RequestBody Map<String, String> map) {
@@ -33,7 +37,7 @@ public class TopicController {
                 LocalDateTime.now()
         );
 
-        topicRepository.insert(topic);
+        topicRepository.save(topic);
 
         var textBlock = new TextBlock.Post(
                 null,
@@ -45,9 +49,103 @@ public class TopicController {
                 Lists.newArrayList(),
                 Lists.newArrayList(),
                 Lists.newArrayList()
-                );
+        );
 
         postRepository.save(textBlock);
+
+        return "";
+    }
+
+    @PostMapping("/reply_topic")
+    public String replyTopic(@RequestBody Map<String, String> map) {
+
+        var topic = new Topic(
+                null,
+                map.get("boardId"),
+                null, //don't have a post id now.
+                map.get("title"),
+                false,
+                1,
+                LocalDateTime.now()
+        );
+
+        topicRepository.save(topic);
+
+        var textBlock = new TextBlock.Reply(
+                null,
+                topic.getId(),
+                topic.getPosterId(),
+                topic.getCreateDateTime(),
+                topic.getCreateDateTime(),
+                map.get("topicContent"),
+                Lists.newArrayList(),
+                Lists.newArrayList(),
+                Lists.newArrayList()
+        );
+
+        replyRepository.save(textBlock);
+
+        return "";
+    }
+
+    @PostMapping("/comment_post")
+    public String commentPost(@RequestBody Map<String, String> map) {
+
+        var post = postRepository.findById(map.get("id"));
+        if (post.isPresent()) {
+
+            var textBlock = new TextBlock.Comment(
+                    null,
+                    post.get().getTopicId(),
+                    post.get().getOwnerId(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    map.get("commentContent"),
+                    Lists.newArrayList(),
+                    Lists.newArrayList()
+            );
+
+            var commentList = post.get().getComments();
+            commentList.add(textBlock);
+            post.get().setComments(commentList);
+
+            postRepository.save(post.get());
+
+        } else {
+
+            return "";
+        }
+
+        return "";
+    }
+
+    @PostMapping("/comment_reply")
+    public String commentReply(@RequestBody Map<String, String> map) {
+
+        var reply = replyRepository.findById(map.get("id"));
+        if (reply.isPresent()) {
+
+            var textBlock = new TextBlock.Comment(
+                    null,
+                    reply.get().getTopicId(),
+                    reply.get().getOwnerId(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    map.get("commentContent"),
+                    Lists.newArrayList(),
+                    Lists.newArrayList()
+            );
+
+            var commentList = reply.get().getComments();
+            commentList.add(textBlock);
+            reply.get().setComments(commentList);
+
+            replyRepository.save(reply.get());
+
+        } else {
+
+            return "";
+        }
 
         return "";
     }
--- src/main/java/org/discu2/forum/repository/PostRepository.java
+++ src/main/java/org/discu2/forum/repository/PostRepository.java
@@ -3,6 +3,10 @@ package org.discu2.forum.repository;
 import org.discu2.forum.model.TextBlock;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends MongoRepository<TextBlock.Post, String> {
+
+    Optional<TextBlock.Post> findById(String id);
 
 }
