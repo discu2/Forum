4faf8e4126babfea28e7828e1563bfe1a8d87d4d
WWX <52891597+asdkmm5050@users.noreply.github.com>
2022-02-25 04:05:38 +0800

    implements basic TopicController functions (postTopic, replyTopic, commentPost and commentReply). (#9)

--- src/main/java/org/discu2/forum/repository/TopicRepository.java
+++ src/main/java/org/discu2/forum/repository/TopicRepository.java
@@ -3,5 +3,8 @@ package org.discu2.forum.repository;
 import org.discu2.forum.model.Topic;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface TopicRepository extends MongoRepository<Topic, String> {
+
 }
