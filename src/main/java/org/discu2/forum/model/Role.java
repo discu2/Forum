7e16ce14c5517c7544a73051bab9d328f58c9e27
frank89722 <frank89722@gmail.com>
2022-02-27 18:11:26 +0800

    change role model again (i believe this is the last time)

--- src/main/java/org/discu2/forum/model/Role.java
+++ src/main/java/org/discu2/forum/model/Role.java
@@ -6,27 +6,18 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.*;
 
 @Data
 @AllArgsConstructor
 @Document
 public class Role {
 
-    private static final String[] PERMISSIONS = new String[] { "access", "post", "reply", "comment", "edit_post", "edit_reply", "edit_comment", "moderate" };
-
     @Id
     private String id;
 
     private String name;
 
-    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
-
-        var authorities = new HashSet<SimpleGrantedAuthority>();
-        for (var p : PERMISSIONS) authorities.add(new SimpleGrantedAuthority(p + "_" + name));
-
-        if (name.equals("DEFAULT")) authorities.add(new SimpleGrantedAuthority("account_self"));
-
-        return authorities;
+    public SimpleGrantedAuthority getGrantedAuthorities() {
+        return new SimpleGrantedAuthority(name);
     }
 }
