6aad18be77640e46be62a4e14e41d93d833b0fc8
frank89722 <frank89722@gmail.com>
2022-03-03 00:05:13 +0800

    Changes by asdkmm5050 - remove unused import

--- src/main/java/org/discu2/forum/model/Topic.java
+++ src/main/java/org/discu2/forum/model/Topic.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @Document
