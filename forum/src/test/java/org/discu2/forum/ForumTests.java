bb92916ffa7000ed873b08d68aee41fac626a625
frank89722 <frank89722@gmail.com>
2022-03-23 21:31:27 +0800

    make forum as a sub module

--- forum/src/test/java/org/discu2/forum/ForumTests.java
+++ forum/src/test/java/org/discu2/forum/ForumTests.java
@@ -3,9 +3,11 @@ package org.discu2.forum;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 class ForumTests {
 
--- forum/src/test/java/org/discu2/forum/controller/AccountControllerTest.java
+++ forum/src/test/java/org/discu2/forum/controller/AccountControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @SpringBootTest
 class AccountControllerTest {
 
