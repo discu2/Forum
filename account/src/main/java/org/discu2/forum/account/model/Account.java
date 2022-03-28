1a86b1896231b256a6659c2e83597b80bcb38f94
frank89722 <frank89722@gmail.com>
2022-03-28 20:48:58 +0800

    define our own UserDetails interface for Accout model

--- account/src/main/java/org/discu2/forum/account/model/Account.java
+++ account/src/main/java/org/discu2/forum/account/model/Account.java
@@ -6,12 +6,12 @@ import lombok.Data;
 import org.discu2.forum.account.repository.AccountRepository;
 import org.discu2.forum.account.repository.RoleRepository;
 import org.discu2.forum.account.util.SpringContext;
+import org.discu2.forum.common.model.IAccount;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -22,7 +22,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Data
 @AllArgsConstructor
 @Document
-public class Account implements UserDetails {
+public class Account implements IAccount {
 
     public static final int MAX_REFRESH_TOKENS = 6;
     private static final RoleRepository roleRepository = SpringContext.getBean(RoleRepository.class);
